185fea87a57561bd09bf2e1e5fc0d41dff972f0d
BlouFlin <bluefeline.mail@gmail.com>
2024-06-17 12:50:47 +0200

    Fix #3 and improved fix for #4
    - The mod disables now the use of blocks / items / entities by checking whether the player using something is actually using the camera, and no longer simply checks whether the client (which is confused with the server for the LAN host player) is marked as using the camera.
    - The mod disables now the use of blocks/items/entities by simply checking whether the player using something has a camera in the main-hand, which solves a rare problem where the item in the off-hand is used anyway due to too tight a timing between checking camera use and marking camera use.

--- src/main/java/net/blouflin/photography/PhotographyClient.java
+++ src/main/java/net/blouflin/photography/PhotographyClient.java
@@ -4,6 +4,7 @@ import net.blouflin.photography.client.PhotographyHud;
 import net.blouflin.photography.networking.CreatePicturePayload;
 import net.blouflin.photography.networking.GetUsingPhotographyCameraPayload;
 import net.blouflin.photography.networking.PlayCameraShutterSoundPayload;
+import net.blouflin.photography.player.PlayerIsUsingCamera;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -16,11 +17,13 @@ import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.item.Items;
+import net.minecraft.util.*;
+
+import java.util.Objects;
 
 @Environment(EnvType.CLIENT)
 public class PhotographyClient implements ClientModInitializer {
@@ -41,26 +44,46 @@ public class PhotographyClient implements ClientModInitializer {
 
         HudRenderCallback.EVENT.register(this::onHudRender);
 
+        ItemStack photographyCameraStack = new ItemStack(Items.SPYGLASS);
+        photographyCameraStack.apply(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT, comp -> comp.apply(currentNbt -> {
+            currentNbt.putBoolean("isPhotographyCamera",true);
+        }));
+
         UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-            if (PhotographyHud.isUsingPhotographyCamera) {
+            if (((PlayerIsUsingCamera) player).isUsingPhotographyCamera()) {
                 player.getStackInHand(Hand.valueOf(PhotographyHud.handUsingPhotographyCamera)).use(world, player, Hand.valueOf(PhotographyHud.handUsingPhotographyCamera));
                 return ActionResult.FAIL;
+            } else if (player.getStackInHand(Hand.MAIN_HAND).getItem() == photographyCameraStack.getItem()) {
+                if (Objects.equals(player.getStackInHand(Hand.MAIN_HAND).getComponents().get(DataComponentTypes.CUSTOM_DATA), photographyCameraStack.getComponents().get(DataComponentTypes.CUSTOM_DATA))) {
+                    player.getStackInHand(Hand.MAIN_HAND).use(world, player, Hand.MAIN_HAND);
+                    return ActionResult.FAIL;
+                }
             }
             return ActionResult.PASS;
         });
 
         UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
-            if (PhotographyHud.isUsingPhotographyCamera) {
+            if (((PlayerIsUsingCamera) player).isUsingPhotographyCamera()) {
                 player.getStackInHand(Hand.valueOf(PhotographyHud.handUsingPhotographyCamera)).use(world, player, Hand.valueOf(PhotographyHud.handUsingPhotographyCamera));
                 return ActionResult.FAIL;
+            } else if (player.getStackInHand(Hand.MAIN_HAND).getItem() == photographyCameraStack.getItem()) {
+                if (Objects.equals(player.getStackInHand(Hand.MAIN_HAND).getComponents().get(DataComponentTypes.CUSTOM_DATA), photographyCameraStack.getComponents().get(DataComponentTypes.CUSTOM_DATA))) {
+                    player.getStackInHand(Hand.MAIN_HAND).use(world, player, Hand.MAIN_HAND);
+                    return ActionResult.FAIL;
+                }
             }
             return ActionResult.PASS;
         });
 
         UseItemCallback.EVENT.register((player, world, hand) -> {
-            if (PhotographyHud.isUsingPhotographyCamera) {
+            if (((PlayerIsUsingCamera) player).isUsingPhotographyCamera()) {
                 player.getStackInHand(Hand.valueOf(PhotographyHud.handUsingPhotographyCamera)).use(world, player, Hand.valueOf(PhotographyHud.handUsingPhotographyCamera));
                 return TypedActionResult.fail(ItemStack.EMPTY);
+            } else if (player.getStackInHand(Hand.MAIN_HAND).getItem() == photographyCameraStack.getItem()) {
+                if (Objects.equals(player.getStackInHand(Hand.MAIN_HAND).getComponents().get(DataComponentTypes.CUSTOM_DATA), photographyCameraStack.getComponents().get(DataComponentTypes.CUSTOM_DATA))) {
+                    player.getStackInHand(Hand.MAIN_HAND).use(world, player, Hand.MAIN_HAND);
+                    return TypedActionResult.fail(ItemStack.EMPTY);
+                }
             }
             return TypedActionResult.pass(ItemStack.EMPTY);
         });
